a17fa683cecc399621792d01e334e8aca8be9083
melissajimison <melissajimison@gmail.com>
2016-04-20 21:18:11 -0700

    Be able to create a new task:

--- tasklistapp/app/controllers/tasks_controller.rb
+++ tasklistapp/app/controllers/tasks_controller.rb
@@ -1,12 +1,31 @@
 class TasksController < ApplicationController
   def index
     @all_tasks = Task.order(name: :asc)
-
   end
 
   def by_name
-
     @all_tasks = Task.where(name: params[:name])
+  end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_create_params[:task])
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
+    # @task = Task.new
+  end
+
+
+  private
+
+  def tasks_create_params
+    params.permit(task: [:name, :description])
+    # params.permit(album: [:artist, :title])
   end
 end
--- tasklistapp/config/routes.rb
+++ tasklistapp/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'tasks#index'
 
+  get '/tasks/new' => 'tasks#new'
+  post '/tasks' => 'tasks#create', as: 'tasks'
 
   get '/:name' => 'tasks#by_name'
 
