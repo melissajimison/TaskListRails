1dd5362a0677860f9f06a8e84db8c2dcba3d156e
melissajimison <melissajimison@gmail.com>
2016-04-25 15:07:09 -0700

    migrated error corrected

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419204103) do
+ActiveRecord::Schema.define(version: 20160425192841) do
+
+  create_table "people", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name",         null: false
